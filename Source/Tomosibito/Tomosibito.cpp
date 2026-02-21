10b862c29c7ba6cf295a42dc7dd1629dcbcc9cc8
UE5team <Tca2304S70001@edu.tca.ac.jp>
2026-02-21 12:48:05 +0900

    初期データ

---
+++ Source/Tomosibito/Tomosibito.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Tomosibito.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Tomosibito, "Tomosibito" );
+
+DEFINE_LOG_CATEGORY(LogTomosibito)
---
+++ Source/Tomosibito/TomosibitoGameMode.cpp
@@ -0,0 +1,8 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "TomosibitoGameMode.h"
+
+ATomosibitoGameMode::ATomosibitoGameMode()
+{
+	// stub
+}
